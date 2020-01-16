79a2790e43b97ecddf682c851c5113dc18f55906
CallumJWilson <callum.j.wilson@strath.ac.uk>
2020-01-16 12:51:31 +0000

    adding normal qnet agent

--- ELM-RL/EQLM/networks.py
+++ ELM-RL/EQLM/networks.py
@@ -96,5 +96,68 @@ class ELMNet():
 			self.step_count=0
 	
 class QNet():
-	def __init__(self,agent_config,network_config,env):
-		pass
+	def __init__(self, state_size, action_size,
+				N_hid=None, alpha=0.01, activation_function='tanh', update_steps=50, clip_norm=None,
+				w_init_magnitude=0.001, b_init_magnitude=0.001, minibatch_size=5, **kwargs):
+		"""
+		A network which uses gradient-based updates
+		"""
+		N_hid = action_size if N_hid is None else N_hid
+		# build network
+		self.s_input = tf.placeholder(shape=[None,state_size],dtype=tf.float32)
+		self.w_in = tf.Variable(tf.random_uniform([state_size,N_hid],-w_init_magnitude,w_init_magnitude))
+		self.b_in = tf.Variable(tf.random_uniform([1,N_hid],0,b_init_magnitude))
+		self.W = tf.Variable(tf.random_uniform([N_hid,action_size],0,1))
+		try:
+			act_fn = tf.keras.activations.get(activation_function)
+		except ValueError:
+			act_fn = tf.tanh
+		self.act = act_fn(tf.add(tf.matmul(self.s_input,self.w_in),self.b_in))
+		self.Q_est = tf.matmul(self.act,self.W)
+		
+		# Update rules
+		self.prep_state = None
+		self.k = minibatch_size
+		self.nextQ = tf.placeholder(shape=[None,action_size],dtype=tf.float32)
+		loss = tf.reduce_sum(tf.square(self.nextQ - self.Q_est))
+		trainer = tf.train.RMSPropOptimizer(alpha)
+		if clip_norm is not None:
+			grads = trainer.compute_gradients(loss,[self.W,self.w_in,self.b_in])
+			cap_grads = [(tf.clip_by_norm(grad, clip_norm), var) for grad, var in grads]
+			self.updateModel = trainer.apply_gradients(cap_grads)
+		else:
+			self.updateModel = trainer.minimize(loss,var_list=[self.W,self.w_in,self.b_in])
+
+		# Target network
+		self.wt = tf.Variable(tf.random_uniform([state_size,N_hid],0,0.01))
+		self.bt = tf.Variable(tf.random_uniform([1,N_hid],0,0.01))
+		self.Wt = tf.Variable(tf.random_uniform([N_hid,action_size],0,0.1))
+		self.actt = act_fn(tf.add(tf.matmul(self.s_input,self.wt),self.bt))
+		self.Qt = tf.matmul(self.actt,self.Wt)
+		
+		self.wt_assign = self.wt.assign(self.w_in)
+		self.bt_assign = self.bt.assign(self.b_in)
+		self.Wt_assign = self.Wt.assign(self.W)
+		
+		# Start tensorflow session
+		self.sess = tf.Session()
+		self.sess.run(tf.global_variables_initializer())
+		self.sess.run([self.Wt_assign, self.wt_assign, self.bt_assign])
+		
+		self.step_count = 0
+		self.C = update_steps
+		
+	def Q_predict(self, s):
+		return self.sess.run(self.Q_est,feed_dict={self.s_input:s})
+
+	def Q_target(self, s):
+		return self.sess.run(self.Qt,feed_dict={self.s_input:s})
+
+	def update(self, S, Q):
+		self.sess.run(self.updateModel,{self.state_input:S,self.nextQ:Q})
+
+		# Update target network
+		self.step_count += 1
+		if self.step_count>=self.C:
+			self.sess.run([self.Wt_assign, self.wt_assign, self.bt_assign])
+			self.step_count=0
